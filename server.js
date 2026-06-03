4ac25c521c6ec6c82abf4495de43e24ef4f939f5
Steto <angeloenzzo@gmail.com>
2026-06-03 18:16:23 -0300

    config inicial

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const parth = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(parth.join(__dirname, 'src/public')));
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello World' });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/public', 'index.html'));
+});
+
+if (process.env.NODE_ENV === 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+} else {
+    app.listen(PORT, 'localhost', () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
